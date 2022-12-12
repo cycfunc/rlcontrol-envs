6ef37d782768d02a1bd6dbfe040c3c18cf8f23fd
Yash <ysg22@cam.ac.uk>
2022-12-12 18:42:48 +0000

    schtufffff

--- control_envs/__init__.py
+++ control_envs/__init__.py
@@ -1,9 +1,11 @@
-from gym.envs.registration import load_env_plugins as _load_env_plugins
-from gym.envs.registration import make, register, registry, spec
+# from gym.envs.registration import load_env_plugins as _load_env_plugins
+# from gym.envs.registration import make, register, registry, spec
 
-# Hook to load plugins from entry points
-_load_env_plugins()
-### TODO: remove above?
+# # Hook to load plugins from entry points
+# _load_env_plugins()
+# ### TODO: remove above?
+
+from gym.envs.registration import register
 
 
 # Classic
