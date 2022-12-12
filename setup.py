c3a50bcad3e71784ab9576893c0c70ec265879c5
Yash <ysg22@cam.ac.uk>
2022-12-12 17:29:16 +0000

    changed setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="control_envs",
     version="0.0.1",
-    install_requires=["gym==0.26.2", "pygame==2.1.0"],
+    install_requires=["gym==0.21", "pygame==2.1.0"],
 )
 
 # TODO: tweak this?
